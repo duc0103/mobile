68dd4797ec9d8eafd867c42f1acb0a4eb5bfe6b0
duc0103 <leducdhcn@gmail.com>
2021-04-23 12:32:46 +0700

    SatefullWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
+
 void main(){
   runApp(new Application());
 }
 
-class Application extends StatelessWidget {
+class Application extends StatefulWidget {
+  @override
+  _ApplicationState createState() => new _ApplicationState();
+}
+
+
+
+
+class _ApplicationState extends State<Application>  {
+
+  String text ='';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    text = 'click button';
+    super.initState();
+  }
+
+  void method1(){
+    setState((){
+      text = ' Changed';
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "Stateless Widget",
       home: new Scaffold(
-          body: new Container(
-            color: Colors.pink,
-            child: new Container(
-              color: Colors.yellow,
-              margin: const EdgeInsets.all(30.0),
-            ),
-          )
-
+        body: new Center(
+          child: new RaisedButton(onPressed:(){method1();},child: new Text(text),),
+        ),
       ),
     );
   }
