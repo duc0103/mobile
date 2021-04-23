1b265ea722729cc2ccf79d80271d2da96f9409b9
duc0103 <leducdhcn@gmail.com>
2021-04-23 11:03:43 +0700

    rowandcolumn

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,20 @@ class Application extends StatelessWidget {
     return new MaterialApp(
       title: 'Container',
       home: new Scaffold(
-          body: new Container(
-            padding: const EdgeInsets.all(50.0),
-            // color: Colors.amber,
-            margin: const EdgeInsets.all(20.0),
-            // width: 200.0,
-            // height: 200.0,
-            child: new Text('This is container'),
-            alignment:Alignment.topRight,
-            decoration:new BoxDecoration(
-                color: Colors.amber,
-                boxShadow:[const BoxShadow(color:const Color(0xFF000000),offset: Offset.zero,blurRadius: 5.0),],
-                gradient: new LinearGradient(colors: [Colors.brown,Colors.amber],)
-            ),
-            // foregroundDecoration: new BoxDecoration(color: Colors.blue) ,
+          body: new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text('this'),
+              new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Text('this'),
+                  new Text('is'),
+                  new Text('column'),
+                ],
+              ),
+              new Text('Row'),
+            ],
           )
       ),
     );
