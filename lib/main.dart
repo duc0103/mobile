d77d26674a267f43307f8988eb6d7f9fca4f2764
duc0103 <leducdhcn@gmail.com>
2021-04-23 12:40:09 +0700

    container

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(new Application());
-}
+void main() => runApp(new Application());
 
-class Application extends StatefulWidget {
-  @override
-  _ApplicationState createState() => new _ApplicationState();
-}
-
-
-
-
-class _ApplicationState extends State<Application>  {
-
-  String text ='';
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    text = 'click button';
-    super.initState();
-  }
-
-  void method1(){
-    setState((){
-      text = ' Changed';
-    });
-  }
-
-
+class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      title: 'Container',
       home: new Scaffold(
-        body: new Center(
-          child: new RaisedButton(onPressed:(){method1();},child: new Text(text),),
-        ),
+          body: new Container(
+            padding: const EdgeInsets.all(50.0),
+            // color: Colors.amber,
+            margin: const EdgeInsets.all(20.0),
+            // width: 200.0,
+            // height: 200.0,
+            child: new Text('This is container'),
+            alignment:Alignment.topRight,
+            decoration:new BoxDecoration(
+                color: Colors.amber,
+                boxShadow:[const BoxShadow(color:const Color(0xFF000000),offset: Offset.zero,blurRadius: 5.0),],
+                gradient: new LinearGradient(colors: [Colors.brown,Colors.amber],)
+            ),
+            // foregroundDecoration: new BoxDecoration(color: Colors.blue) ,
+          )
       ),
     );
   }
